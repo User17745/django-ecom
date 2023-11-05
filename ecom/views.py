8a5cdbc6b976309a2dc8dfddf467c23cf8574dfc
User17745 <abhishekaggarwal1995@gmail.com>
2023-11-05 19:01:41 +0530

    Added Header Menu

--- ecom/views.py
+++ ecom/views.py
@@ -7,11 +7,12 @@ import requests, json
 def index(request, cat_url="winter-wear", page_no=1):
     per_page_product_count = 16
 
+    cat_tree = call_api("https://getketchpim.getketch.com/pim/pimresponse.php/?service=menu&store=1");
     plp = call_api(f"https://getketchpim.getketch.com/pim/pimresponse.php/?service=category&store=1&url_key={cat_url}&page={page_no}&count={per_page_product_count}&sort_by=&sort_dir=desc&filter=");
     # return HttpResponse(json.dumps(plp))
 
     # total_page_count = plp.result.count / per_page_product_count;
-    context = {"callback" : plp, "cat_url": cat_url, "current_page_no": page_no, "next_page_no": page_no+1, "prev_page_no": page_no-1, "total_page_count": 100} 
+    context = {"cat_tree" : cat_tree, "callback" : plp, "cat_url": cat_url, "current_page_no": page_no, "next_page_no": page_no+1, "prev_page_no": page_no-1, "total_page_count": 100} 
     return render(request, "ecom/index.html", context)
 
 def call_api(api_url):
@@ -19,15 +20,12 @@ def call_api(api_url):
         response = requests.get(api_url)
 
         if response.status_code == 200:
-            data = response.json()  # Parse the JSON response
-            # Process the data as needed
+            data = response.json()
             return data
         else:
-            # Handle errors appropriately
             print(f"API request failed with status code {response.status_code}")
             return None
 
     except requests.exceptions.RequestException as e:
-        # Handle network or request errors
         print(f"Request exception: {e}")
         return None
